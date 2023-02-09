4d739901c04a726e002141f1ff6d12fc5cadf84e
longxiang <2782000longxiang@gmail.com>
2023-02-09 19:45:04 +0100

    empezando a implementar la clase person

--- src/main/java/org/ejercicio/person/Person.java
+++ src/main/java/org/ejercicio/person/Person.java
@@ -1,4 +1,21 @@
 package org.ejercicio.person;
 
+/**
+ * Class representing a person with a name, age and gender,
+ *
+ * @author longxiang
+ */
 public class Person {
+
+    private final String name;
+    private final int age;
+    private final String gender;
+
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    
 }
